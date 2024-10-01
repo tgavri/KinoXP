d4afdb40d9e98b0e49e5582d22832fa47d4d7e1b
JannickHansen <147395772+JannickHansen@users.noreply.github.com>
2024-10-01 13:16:42 +0200

    initial project files

---
+++ src/test/java/dk/kea/kinoxp/KinoXpApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.kinoxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
