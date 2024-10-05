309ada78a007fe6460023bceffe7ec0c7f7ad7c0
tgavri <thom54s8@stud.kea.dk>
2024-10-05 02:30:39 +0200

    Dockerfile compose.yaml fix. Kør med terminal "docker compose build" og "docker compose up -d" & (docker compose down) rettet de fleste href links da de nu behøver en controller, oprettet nødv. controllere, stor rettelse i POM fil (nye dependencies - og opdateret tomcat embed da den skabte conflicts - youtube embeds virker stadig), flyttet alle frontend templates i fragments og andre filer(links rettet), så de kunne ses af SpringBoot, rettet Ø i rødegardiner, da det ik er understøttet, mm..
    TO DO: Tjek om nogle links mangler, alt burde virke, men måske jeg har overset noget.

---
+++ src/main/java/dk/kea/kinoxp/FragtmentController.java
@@ -0,0 +1,26 @@
+package dk.kea.kinoxp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FragtmentController {
+    @GetMapping("/fragments/header")
+    public String getIndexHeader() {
+        return "fragments/header";
+    }
+
+    @GetMapping("/fragments/cookies")
+    public String getCookies() {
+        return "fragments/cookies";
+    }
+    @GetMapping("/upcoming")
+    public String showUpcoming() {
+        return "upcoming";
+    }
+    @GetMapping("/cookiesshow")
+    public String showCookiesInfo() {
+        return "CookiesShow";
+    }
+}
+
