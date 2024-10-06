df0b7635df66181eb4b05535f68ad574ae27ee99
JannickHansen <jaha0004@stud.kea.dk>
2024-10-06 19:23:57 +0200

    changes to:
    datahandler - i kan nu kalde get metoden i kino controller til at få adgang til alt ticket data. den returner ticket objects til jer.
    
    seatpickers viser nu røde sæder fra tickets og kan ikke længere vælges.

--- src/main/java/dk/kea/kinoxp/DataHandler.java
+++ src/main/java/dk/kea/kinoxp/DataHandler.java
@@ -1,10 +1,11 @@
 package dk.kea.kinoxp;
 
-import org.json.JSONException;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -29,5 +30,23 @@ public class DataHandler {
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public JSONArray getAllTickets() { // hent alle biletter (hvis det skal bruges i en javaklasse)
+        JSONArray tickets = new JSONArray();
+
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(TICKET_FILE_PATH)));
+            JSONObject json = new JSONObject(content);
+
+            tickets = json.getJSONArray("tickets");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        return tickets;
     }
 }
