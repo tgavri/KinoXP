80c4f113cb74525794c446be8ff550eaf691296d
Keragh <147395749+Keragh@users.noreply.github.com>
2024-10-09 14:19:32 +0200

    tilføjet til controller

--- src/main/java/dk/kea/kinoxp/FragtmentController.java
+++ src/main/java/dk/kea/kinoxp/FragtmentController.java
@@ -1,14 +1,11 @@
 package dk.kea.kinoxp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FragtmentController {
-    private List<Shift> shifts = new ArrayList<>();
+
     @GetMapping("/fragments/header")
     public String getIndexHeader() {
         return "fragments/header";
@@ -30,27 +27,9 @@ public class FragtmentController {
     public String OmOs() {
         return "OmOs";
     }
-    @GetMapping("/shifts/all")
-    @ResponseBody
-    public String getAllShifts() {
-        return "workSchedule";
-    }
 
-    @PostMapping("/shifts/add")
-    @ResponseBody
-    public void addShift(@RequestBody Shift shift) {
-        shifts.add(shift);
-        System.out.println("Shift added: " + shift);
-    }
-
-
-    @DeleteMapping("/shifts/clear")
-    @ResponseBody
-    public void clearShifts() {
-        shifts.clear();
-        System.out.println("All shifts cleared.");
-    }
-
+    @GetMapping("/Arbejdsuge")
+    public String arbejdsuge() {return "workSchedule";}
 
 
 }
